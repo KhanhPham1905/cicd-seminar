8937e52def098a9c96682eb1c18bded2ba5ba4d2
khanhdev <khanhquocphamdev@gmail.com>
2025-08-13 10:03:31 +0700

    cicd

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def add_numbers():
     try:
         num1 = float(request.args.get('num1', 0))
-        print("Hello Khanh, world!, new code. test cicd")
+        print("Hello Khanh, world!, new code")
         print("Hello Khanh, world!, test cicd")
         num2 = float(request.args.get('num2', 0))
         return jsonify({'result khanhdzcheck': num1 * num2})
